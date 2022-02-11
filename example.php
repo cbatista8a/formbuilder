b97fbcd5a966f78427c97156ab48ded0d5dbf027
Carlos Batista <cbatista8a@gmail.com>
2022-02-11 23:38:59 +0100

    update example.php with last changes

--- example.php
+++ example.php
@@ -3,9 +3,14 @@
 require_once './vendor/autoload.php';
 
 use Cbatista8a\Formbuilder\Classes\FormBuilder;
+use Cbatista8a\Formbuilder\Classes\Button;
+use Cbatista8a\Formbuilder\Classes\HttpMethod;
 use Cbatista8a\Formbuilder\Model\Model;
 
-
-$form = new FormBuilder(new Model(),'form-test', 'Envia', 'form-group');
-
+$model = new Model();
+$form = new FormBuilder('form-test', 'example.php', HttpMethod::POST, 'form-group');
+foreach ($model->getFormFields() as $field){
+    $form->addElement($field, 'group-model','row');
+}
+$form->addElement(new Button('button','submit','btn-submit','Submit'),'form-footer');
 echo $form->build();
