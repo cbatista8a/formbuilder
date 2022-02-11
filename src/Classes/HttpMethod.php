a12382923edb3a48ee4dfae5954b9fe77fccb057
Carlos Batista <cbatista8a@gmail.com>
2022-02-11 23:21:37 +0100

    add class HttpMethod

---
+++ src/Classes/HttpMethod.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Cbatista8a\Formbuilder\Classes;
+
+
+class HttpMethod
+{
+
+    public const POST = 'POST';
+    public const GET = 'GET';
+    public const UPDATE = 'UPDATE';
+    public const DELETE = 'DELETE';
+    public const HEAD = 'HEAD';
+}
